1532f1fb66f9eb80c87fa84ba2cfea73e526cc96
francescofalcone05 <ciccio_97@live.it>
2024-07-17 12:57:23 +0200

    languages table, model, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TypeSeeder::class,
             ProjectTableSeeder::class,
+            LanguageSeeder::class,
         ]);
     }
 }
